73b8c9471e8ad489f702378b136e27c3f31f9d48
frantic-dev <107622593+frantic-dev@users.noreply.github.com>
2023-06-27 12:33:08 +0100

    commenting on last commit's logic

--- src/Components/Images.js
+++ src/Components/Images.js
@@ -26,31 +26,39 @@ const allImages = [
   zenitsu,
 ];
 function shuffleImages() {
+  // makes an array of numbers from 0 to images.length
   let spotsLeft = Array.from(
     { length: allImages.length },
     (spot, index) => (spot = index)
   );
+  // empty array to fill with images in new spots
   let shuffledImages = [];
   allImages.forEach((image) => {
     let spotsObject = findNextSpot(spotsLeft);
+    // fills shuffled images and updates the spotsLeft
     if (spotsObject !== undefined) {
       shuffledImages[spotsObject.found] = image;
       spotsLeft = spotsObject.spotsLeft;
     }
   });
-  return shuffledImages
+  return shuffledImages;
 }
+// returns a random number
 function getRandomNumber(length) {
   return Math.floor(Math.random() * length);
 }
 function findNextSpot(spotsLeft) {
   let randomSpot = getRandomNumber(allImages.length);
+  // checks if randomSpot is still available in the spotsLeft array
   let found = spotsLeft.find((spot) => spot === randomSpot);
+  // if it's not : we try to find a new spot again
   if (found === undefined) return findNextSpot(spotsLeft);
   else {
+    // if yes : we filter it out as its not gonna become an available spot anymore
     spotsLeft = spotsLeft.filter((spot) => {
       return spot !== found;
     });
+    // return of found which is the index of new spot, and an updated spotsLeft
     return { found, spotsLeft };
   }
 }
